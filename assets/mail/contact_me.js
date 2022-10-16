3dbcfb6b99a124a622fb9b16c0fc6a5ada3e907e
Thabo Thandekiso <thandekisothabo@gmail.com>
2022-10-16 23:02:04 +0200

    change API URL

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://rci2uk41y3.execute-api.eu-west-1.amazonaws.com",
+                url: "https://2gs0dl8m2m.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
